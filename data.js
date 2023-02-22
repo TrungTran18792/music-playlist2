95b0c2b6f34fb10b699f43b77ea3898e9de42f65
Trung Tran <trungtran535378@gmail.com>
2023-02-23 01:30:52 +0700

    upload code

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Đâu ai dám hứa',
+        path: './music/ĐÂU AI DÁM HỨA  CZEE .mp3',
+        artist: 'CZEE',
+        cover: './img/IMG_7334.png'
+    },
+    {
+        name: 'Ở đây có anh này',
+        path: './music/Ở đây có anh này em thật nhỏ bé trong chiếc ôm này.mp3',
+        artist: 'O.lew',
+        cover: './img/IMG_7719.jpg'
+    },
+    {
+        name: 'Thích anh rồi đấy',
+        path: './music/Thích Anh Rồi Đấy Remix.mp3',
+        artist: 'HANNIE',
+        cover: './img/IMG_7747.jpg'
+    }
+]
